feb1d42e4c20a1d18a63a06705a20bd4347551d8
erikcorrea444@gmail.com <erikcorrea444@gmail.com>
2024-11-12 08:52:45 -0500

    language tests

--- hw/hello.go
+++ hw/hello.go
@@ -2,15 +2,31 @@ package main
 
 import "fmt"
 
+const spanish = "Spanish"
+const french = "French"
+const frenchHelloPrefix = "Bonjour, "
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	return englishHelloPrefix + name
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
 }
 
 func main() {
-	fmt.Println(Hello("Chirss"))
+	fmt.Println(Hello("Chirss", "French"))
 }
